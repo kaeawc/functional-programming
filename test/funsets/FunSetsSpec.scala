27f5bf6125ed645d909241f303f8e90a7e431971
Jason Pearson <jason.d.pearson@gmail.com>
2013-10-22 22:19:22 -0400

    added more tests for functional sets assignment

--- test/funsets/FunSetsSpec.scala
+++ test/funsets/FunSetsSpec.scala
@@ -14,10 +14,28 @@ class FunSetsSpec extends Specification {
 
   import FunSets._
 
-  trait TestSets {
+  trait TestSet {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+  }
+
+  trait TestUnion extends TestSet {
+    val u1 = union(s1,s2)
+    val u2 = union(s2,s3)
+    val u3 = union(s1,s3)
+  }
+
+  trait TestInsersect extends TestUnion {
+    val i1 = intersect(u1,u2)
+    val i2 = intersect(u2,u3)
+    val i3 = intersect(u1,u3)
+  }
+
+  trait TestDiff extends TestUnion {
+    val d1 = diff(u1,u2)
+    val d2 = diff(u2,s1)
+    val d3 = diff(s1,u3)
   }
 
   "FunSets" should {
@@ -29,25 +47,67 @@ class FunSetsSpec extends Specification {
     "singletonSet(1) contains 1" in new WithApplication {
       
       /**
-       * We create a new instance of the "TestSets" trait, this gives us access
+       * We create a new instance of the "TestSet" trait, this gives us access
        * to the values "s1" to "s3". 
        */
-      new TestSets {
+      new TestSet {
         /**
          * The string argument of "assert" is a message that is printed in case
          * the test fails. This helps identifying which assertion failed.
          */
-        contains(s1, 1) mustEqual "Singleton"
+        contains(s1, 1) mustEqual true
       }
-    }.pendingUntilFixed("| Not Yet Implemented")
+    }
 
     "union contains all elements" in new WithApplication {
-      new TestSets {
-        val s = union(s1, s2)
-        contains(s, 1) mustEqual true
-        contains(s, 2) mustEqual true
-        contains(s, 3) mustEqual false
+      new TestUnion {
+
+        contains(u1, 1) mustEqual true
+        contains(u1, 2) mustEqual true
+        contains(u1, 3) mustEqual false
+
+        contains(u2, 1) mustEqual false
+        contains(u2, 2) mustEqual true
+        contains(u2, 3) mustEqual true
+
+        contains(u3, 1) mustEqual true
+        contains(u3, 2) mustEqual false
+        contains(u3, 3) mustEqual true
       }
-    }.pendingUntilFixed("| Not Yet Implemented")
+    }
+
+    "intersect contains all elements from both" in new WithApplication {
+      new TestInsersect {
+
+        contains(i1, 1) mustEqual false
+        contains(i1, 2) mustEqual true
+        contains(i1, 3) mustEqual false
+
+        contains(i2, 1) mustEqual false
+        contains(i2, 2) mustEqual false
+        contains(i2, 3) mustEqual true
+
+        contains(i3, 1) mustEqual true
+        contains(i3, 2) mustEqual false
+        contains(i3, 3) mustEqual false
+      }
+    }
+
+    "diff contains only elements not in one or the other" in new WithApplication {
+      new TestDiff {
+
+        contains(d1, 1) mustEqual true
+        contains(d1, 2) mustEqual false
+        contains(d1, 3) mustEqual true
+
+        contains(d2, 1) mustEqual true
+        contains(d2, 2) mustEqual true
+        contains(d2, 3) mustEqual true
+
+        contains(d3, 1) mustEqual false
+        contains(d3, 2) mustEqual false
+        contains(d3, 3) mustEqual true
+      }
+    }
   }
 }
